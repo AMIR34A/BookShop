863a12a4e29b9dc5482f5e4ea0e54f404c6bfe69
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-08-29 21:24:21 +0330

    Added ApplicationIdentityErrorDescriber.

---
+++ BookShop/Areas/Identity/Data/ApplicationIdentityErrorDescriber.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BookShop.Areas.Identity.Data;
+
+public class ApplicationIdentityErrorDescriber : IdentityErrorDescriber
+{
+    public override IdentityError PasswordRequiresNonAlphanumeric()
+    {
+        return base.PasswordRequiresNonAlphanumeric();
+    }
+}
