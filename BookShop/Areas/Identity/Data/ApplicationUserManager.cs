c3bd3e6da2de981d45f0067d3ed6bb8deaf03ecc
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-09-18 19:47:15 +0330

    Added IApplicationUserManager and ApplicationUserManager.

---
+++ BookShop/Areas/Identity/Data/ApplicationUserManager.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BookShop.Areas.Identity.Data;
+
+public class ApplicationUserManager : UserManager<ApplicationUser> ,IApplicationUserManager
+{
+    private readonly IUserStore<ApplicationUser> _store;
+    private readonly IOptions<IdentityOptions> _optionsAccessor;
+    private readonly IPasswordHasher<ApplicationUser> _passwordHasher;
+    private readonly IEnumerable<IUserValidator<ApplicationUser>> _userValidators;
+    private readonly IEnumerable<IPasswordValidator<ApplicationUser>> _passwordValidators;
+    private readonly ILookupNormalizer _keyNormalizer;
+    private readonly ApplicationIdentityErrorDescriber _errors;
+    private readonly IServiceProvider _services;
+    private readonly ILogger<ApplicationUserManager> _logger;
+    public ApplicationUserManager(
+        IUserStore<ApplicationUser> store,
+        IOptions<IdentityOptions> optionsAccessor,
+        IPasswordHasher<ApplicationUser> passwordHasher,
+        IEnumerable<IUserValidator<ApplicationUser>> userValidators,
+        IEnumerable<IPasswordValidator<ApplicationUser>> passwordValidators,
+        ILookupNormalizer keyNormalizer,
+        ApplicationIdentityErrorDescriber errors,
+        IServiceProvider services,
+        ILogger<ApplicationUserManager> logger) : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger)
+    {
+        _store = store;
+        _optionsAccessor = optionsAccessor;
+        _passwordHasher = passwordHasher;
+        _userValidators = userValidators;
+        _keyNormalizer = keyNormalizer;
+        _errors = errors;
+        _services = services;
+        _logger = logger;
+    }
+
+    public async Task<List<ApplicationUser>> GetAllUsersAsync()
+    {
+        return await Users.ToListAsync();
+    }
+
+    public string NormalizeKey(string key)
+    {
+        throw new NotImplementedException();
+    }
+}
