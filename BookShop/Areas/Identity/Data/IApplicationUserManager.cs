037c47fbf1c0cc745b4408fae81d7f4144180cd8
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-09-25 13:27:59 +0330

    Renamed GetUserWithRolesByIdAsync to FindUserWithRolesByIdAsync

--- BookShop/Areas/Identity/Data/IApplicationUserManager.cs
+++ BookShop/Areas/Identity/Data/IApplicationUserManager.cs
@@ -113,6 +113,6 @@ public interface IApplicationUserManager
     #region CustomMethod
     Task<List<ApplicationUser>> GetAllUsersAsync();
     Task<List<UsersViewModel>> GetAllUsersWithRolesAsync();
-    Task<UsersViewModel> GetUserWithRoleAsync(string id);
+    Task<UsersViewModel> FindUserWithRolesByIdAsync(string id);
     #endregion
 }
