f98226a3541b27bc81f8ea21d1043577e9ec147b
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-09-27 17:40:49 +0330

    Added httppost Edit action method to UsersManagerController

--- BookShop/Areas/Admin/Controllers/UsersManagerController.cs
+++ BookShop/Areas/Admin/Controllers/UsersManagerController.cs
@@ -1,4 +1,6 @@
 ﻿using BookShop.Areas.Identity.Data;
+using BookShop.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ReflectionIT.Mvc.Paging;
 
@@ -20,6 +22,8 @@ public class UsersManagerController : Controller
     {
         if (!string.IsNullOrEmpty(message) && message.Equals("success"))
             ViewBag.Message = "کاربر با موفقیت اضافه شد.";
+        else if (!string.IsNullOrEmpty(message) && message.Equals("SucceededEdit"))
+            ViewBag.Message = "تغیرات با موفقیت ثبت شد.";
 
         var pagingModel = PagingList.Create(await _userManager.GetAllUsersWithRolesAsync(), pageSize, pageIndex);
         pagingModel.RouteValue = new RouteValueDictionary
@@ -45,5 +49,48 @@ public class UsersManagerController : Controller
             return NotFound();
         ViewBag.Roles = _roleManager.GetAllRoles();
         return View(user);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(UsersViewModel userViewModel)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = await _userManager.FindByIdAsync(userViewModel.Id);
+            if (user is null)
+                return NotFound();
+
+            IdentityResult identityResult;
+            var recentRoles = await _userManager.GetRolesAsync(user);
+            var deletedRoles = recentRoles.Except(userViewModel.Roles);
+            var addedRoles = userViewModel.Roles.Except(recentRoles);
+
+            identityResult = await _userManager.RemoveFromRolesAsync(user, deletedRoles);
+            if (identityResult.Succeeded)
+            {
+                identityResult = await _userManager.AddToRolesAsync(user, addedRoles);
+                if (identityResult.Succeeded)
+                {
+                    user.FirstName = userViewModel.FirstName;
+                    user.LastName = userViewModel.LastName;
+                    userViewModel.Username = userViewModel.Username;
+                    user.Email = userViewModel.Email;
+                    user.PhoneNumber = userViewModel.PhoneNumber;
+                    user.BirthDate = userViewModel.BirthDate;
+                }
+                identityResult = await _userManager.UpdateAsync(user);
+                if (identityResult.Succeeded)
+                {
+                    ViewBag.Message = "تغییرات با موفقیت اعمال شد.";
+                    return RedirectToAction("Index", new { message = "SucceededEdit" });
+                }
+            }
+            if (identityResult is null)
+                foreach (var error in identityResult.Errors)
+                    ModelState.AddModelError(error.Code, error.Description);
+        }
+        ViewBag.Roles = _roleManager.GetAllRoles();
+        return View(userViewModel);
     }
 }
