18b56b753505186bbee014a9b9d7ee545763142d
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-09-29 16:25:26 +0330

    make Image nullable

--- BookShop/Areas/Identity/Data/ApplicationUser.cs
+++ BookShop/Areas/Identity/Data/ApplicationUser.cs
@@ -8,7 +8,7 @@ public class ApplicationUser : IdentityUser
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public DateTime BirthDate { get; set; }
-    public string Image { get; set; }
+    public string? Image { get; set; }
     public DateTime RegisterDate { get; set; }
     public DateTime LastViewDateTime { get; set; }
     public bool IsActive { get; set; }
