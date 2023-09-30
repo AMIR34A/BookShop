ab4e89395b9483a995c0e52251a4ee21e4b5c009
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-09-30 17:43:18 +0330

    Add attribute to AccountViewModel

--- BookShop/Models/ViewModels/AccountViewModel.cs
+++ BookShop/Models/ViewModels/AccountViewModel.cs
@@ -1,12 +1,29 @@
-﻿namespace BookShop.Models.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.Models.ViewModels;
 
 public class AccountViewModel
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    [Display(Name ="نام کاربری")]
+    [Required(ErrorMessage ="وارد نمودن {0} الزامی است")]
     public string Username { get; set; }
-    public string Email { get; set; }
-    public string  Password { get; set; }
-    public string ConfirmePassword { get; set; }
 
+    [Display(Name = "ایمیل")]
+    [Required(ErrorMessage = "وارد نمودن {0} الزامی است")]
+    public string Email { get; set; }
+
+    [DataType(DataType.Password)]
+    [Display(Name = "کلمه عبور")]
+    [Required(ErrorMessage = "وارد نمودن {0} الزامی است.")]
+    [StringLength(100, ErrorMessage = "{0} باید دارای حداقل {2} کاراکتر و حداکثر دارای {1} کاراکتر باشد.", MinimumLength = 6)]
+    public string  Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Display(Name = "تکرار کلمه عبور")]
+    [Compare("Password", ErrorMessage = "کلمه عبور وارد شده با تکرار کلمه عبور مطابقت ندارد.")]
+    public string ConfirmPassword { get; set; }
+
+    [Display(Name = "شماره موبایل")]
+    [Required(ErrorMessage = "وارد نمودن {0} الزامی است.")]
+    public string PhoneNumber { get; set; }
 }
