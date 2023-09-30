af7edd7a9d2bbf8b3f7235c94c8559fb07283942
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-09-30 17:35:31 +0330

    Make nullable FirstName, LastName, and Description

--- BookShop/Areas/Identity/Data/ApplicationRole.cs
+++ BookShop/Areas/Identity/Data/ApplicationRole.cs
@@ -10,6 +10,6 @@ public class ApplicationRole : IdentityRole
 
     public ApplicationRole(string name, string description) : base(name) => Description = description;
 
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public virtual List<ApplicationUserRoles> Users { get; set; }
 }
--- BookShop/Areas/Identity/Data/ApplicationUser.cs
+++ BookShop/Areas/Identity/Data/ApplicationUser.cs
@@ -5,8 +5,8 @@ namespace BookShop.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the BookShopUser class
 public class ApplicationUser : IdentityUser
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
     public DateTime? BirthDate { get; set; }
     public string? Image { get; set; }
     public DateTime RegisterDate { get; set; }
