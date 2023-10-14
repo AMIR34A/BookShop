e990a04d09d9d0fec80120ce8ac51d1466354872
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-10-14 13:25:12 +0330

    Make nullable Providers

--- BookShop/Models/ViewModels/AccountViewModel.cs
+++ BookShop/Models/ViewModels/AccountViewModel.cs
@@ -78,7 +78,7 @@ public class ResetPasswordViewModel
 public class SendCodeViewModel
 {
     public string SelectedProvider { get; set; }
-    public ICollection<SelectListItem> Providers { get; set; }
+    public ICollection<SelectListItem>? Providers { get; set; }
     public bool RememberMe { get; set; }
 }
 
