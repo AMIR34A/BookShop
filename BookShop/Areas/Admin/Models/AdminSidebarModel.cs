4cb7cf22e689ae02bfa742cdf2b0586cc5d4e1bd
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-10-17 15:34:25 +0330

    Delete AdminSidebarModel

--- BookShop/Areas/Admin/Models/AdminSidebarModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookShop.Areas.Admin.Models;
-
-public class AdminSidebarModel
-{
-    public string FullName { get; set; }
-    public DateTime? LastVisit { get; set; }
-    public DateTime? RegisterTime { get; set; }
-    public string Image { get; set; }
-}
