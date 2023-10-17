27872ec2fd246bc03138d51b46c489a506a35461
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-10-17 11:10:56 +0330

    Add AdminSidebarModel

---
+++ BookShop/Areas/Admin/Models/AdminSidebarModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BookShop.Areas.Admin.Models;
+
+public class AdminSidebarModel
+{
+    public string FullName { get; set; }
+    public DateTime? LastVisit { get; set; }
+    public DateTime? RegisterTime { get; set; }
+    public string Image { get; set; }
+}
