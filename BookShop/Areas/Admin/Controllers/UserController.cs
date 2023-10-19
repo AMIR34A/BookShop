43256a8151f028edbd81cdbb8f6c339fb50ff818
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-10-19 16:35:31 +0330

    Add EnableAuthenticator action method, FormatKey, and GenerateQRCodeUri

--- BookShop/Areas/Admin/Controllers/UserController.cs
+++ BookShop/Areas/Admin/Controllers/UserController.cs
@@ -1,6 +1,8 @@
-﻿using BookShop.Areas.Identity.Data;
+﻿using BookShop.Areas.Admin.Models.ViewModels;
+using BookShop.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 namespace BookShop.Areas.Admin.Controllers;
 
@@ -8,8 +10,45 @@ public class UserController : Controller
 {
     private readonly IApplicationUserManager _userManager;
     private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly UrlEncoder _urlEncoder;
+
+    public UserController(IApplicationUserManager userManager, SignInManager<ApplicationUser> signInManager, UrlEncoder urlEncoder)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+        urlEncoder = urlEncoder;
+    }
     public IActionResult Index()
     {
         return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> EnableAuthenticator()
+    {
+        var user = await _signInManager.GetTwoFactorAuthenticationUserAsync();
+        if (user is null)
+            return NotFound();
+
+        var unFormattedKey = _userManager.GenerateNewAuthenticatorKey();
+        string formattedKey = FormatKey(unFormattedKey);
+        EnableAuthenticatorViewModel enableAuthenticatorViewModel = new EnableAuthenticatorViewModel
+        {
+            AuthenticatorUri = GenerateQRCodeUri(unFormattedKey, user.Email),
+            SharedKey = FormatKey(formattedKey)
+        };
+        return View(enableAuthenticatorViewModel);
+    }
+
+    public string FormatKey(string key)
+    {
+        var seperated = key.Chunk(4);
+        return string.Join(" ", seperated);
+    }
+
+    public string GenerateQRCodeUri(string unformattedKey, string email)
+    {
+        string authenticatorFormat = "otpauth://totp/{0}/:{1}/?secret={2}/&issuer={0}&digits=6";
+        return string.Format(authenticatorFormat, _urlEncoder.Encode("BookShop"), _urlEncoder.Encode(email), unformattedKey);
     }
 }
