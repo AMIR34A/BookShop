2143894610b828eee26e3d5e802e66c9a04613ac
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-10-19 16:03:59 +0330

    Add necessary services to controller

--- BookShop/Areas/Admin/Controllers/UserController.cs
+++ BookShop/Areas/Admin/Controllers/UserController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookShop.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookShop.Areas.Admin.Controllers;
 
 public class UserController : Controller
 {
+    private readonly IApplicationUserManager _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
     public IActionResult Index()
     {
         return View();
