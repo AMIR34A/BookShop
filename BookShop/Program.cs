6cb9274c648e182136c3350fbebefb079e18d69f
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-10-23 16:44:37 +0330

    Edit Program.cs file and fix a bug

--- BookShop/Program.cs
+++ BookShop/Program.cs
@@ -60,7 +60,6 @@ builder.Services.AddScoped<IApplicationUserManager, ApplicationUserManager>();
 builder.Services.AddScoped<ApplicationIdentityErrorDescriber>();
 builder.Services.AddScoped<IEmailSender, EmailSenderService>();
 builder.Services.AddScoped<ISMSSenderService, SMSSenderService>();
-builder.Services.AddScoped<UrlEncoder>();
 
 builder.Services.AddLocalization(option => option.ResourcesPath = "Resources");
 builder.Services.AddMvc(setup =>
