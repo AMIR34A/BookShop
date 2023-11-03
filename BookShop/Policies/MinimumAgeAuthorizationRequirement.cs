562345e0f47a57ed1b3c3d2fdc4f3c4716dcb2b4
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-11-03 15:16:19 +0330

    Add MinimumAgeAuthorizationRequirement

---
+++ BookShop/Policies/MinimumAgeAuthorizationRequirement.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace BookShop.Policies;
+
+public class MinimumAgeAuthorizationRequirement : IAuthorizationRequirement
+{
+    public MinimumAgeAuthorizationRequirement(int minimumAge)
+    {
+        MinimumAge = minimumAge;
+    }
+
+    public int MinimumAge { get; set; }
+}
