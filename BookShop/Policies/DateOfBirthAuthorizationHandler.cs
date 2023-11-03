08a2a7532e7a2da8eda56297c9a03aeea0ac6387
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-11-03 14:26:11 +0330

    Add DateOfBirthAuthorizationHandler

---
+++ BookShop/Policies/DateOfBirthAuthorizationHandler.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace BookShop.Policies
+{
+    public class DateOfBirthAuthorizationHandler : AuthorizationHandler<DateOfBirthAuthorizationRequirement>
+    {
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, DateOfBirthAuthorizationRequirement requirement)
+        {
+            if (!context.User.HasClaim(claim => claim.Type == ClaimTypes.DateOfBirth))
+                await Task.CompletedTask;
+
+            DateTime dateOfBirth = Convert.ToDateTime(context.User.FindFirstValue(ClaimTypes.DateOfBirth));
+
+            if (DateTime.Compare(new DateTime(DateTime.Now.Year, dateOfBirth.Month, dateOfBirth.Day), DateTime.Today) == 0)
+                context.Succeed(requirement);
+        }
+    }
+}
