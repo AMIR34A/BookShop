b90f3d209760f3648ba1f3e8ff35e6095b5e9e63
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-11-03 16:56:54 +0330

    Add MinimumAgeAuthorizationHandler

---
+++ BookShop/Policies/MinimumAgeAuthorizationHandler.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace BookShop.Policies;
+
+public class MinimumAgeAuthorizationHandler : AuthorizationHandler<MinimumAgeAuthorizationRequirement>
+{
+    protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, MinimumAgeAuthorizationRequirement requirement)
+    {
+        if (!context.User.HasClaim(claim => claim.Type == ClaimTypes.DateOfBirth))
+        {
+            context.Fail();
+            await Task.CompletedTask;
+        }
+        DateTime dateOfBirth = Convert.ToDateTime(context.User.FindFirstValue(ClaimTypes.DateOfBirth));
+        int minimumDays = requirement.MinimumAge * 365;
+        if (DateTime.Now.Subtract(dateOfBirth).TotalDays > minimumDays)
+            context.Succeed(requirement);
+    }
+}
