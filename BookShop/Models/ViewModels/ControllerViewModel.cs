33d11a4b9435ab14b64c9be680e23645f1de5f99
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-11-04 19:03:58 +0330

    Add ControllerViewModel

---
+++ BookShop/Models/ViewModels/ControllerViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShop.Models.ViewModels;
+
+public class ControllerViewModel
+{
+    public string AreaName { get; set; }
+    public IList<Attribute> ControllerAttributes { get; set; }
+    public string ControllerDisplayName { get; set; }
+    public string ControllerName { get; set; }
+    public string ControllerId { get; set; }
+    public IList<ActionViewModel> Actions { get; set; } = new List<ActionViewModel>();
+}
