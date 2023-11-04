7f986c6c4ebf7e012a855dd1345fbd1e7bc36fef
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-11-04 18:53:57 +0330

    Add ActionViewModel

---
+++ BookShop/Models/ViewModels/ActionViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShop.Models.ViewModels;
+
+public class ActionViewModel
+{
+    public IList<Attribute> ActionAttributes { get; set; }
+    public string ActionDisplayName { get; set; }
+    public string ControllerId { get; set; }
+    public string ActionName { get; set; }
+    public string ActionId => $"{ControllerId}:{ActionName}";
+    public bool IsSecuredAction { get; set; }
+}
