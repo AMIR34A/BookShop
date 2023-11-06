ed244576cdf4566070ac925a56cda577c1611c5e
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-11-06 16:19:09 +0330

    Add IMVCActionsDiscoveryService

---
+++ BookShop/Areas/Admin/Services/IMVCActionsDiscoveryService.cs
@@ -0,0 +1,8 @@
+﻿using BookShop.Models.ViewModels;
+
+namespace BookShop.Areas.Admin.Services;
+
+public interface IMVCActionsDiscoveryService
+{
+    ICollection<ControllerViewModel> GetAllSecuredControllerActionsWithPolicy(string policyName);
+}
