12e1463e22c3ac50cdf8f6d975db3e64df37439a
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-11-07 16:57:28 +0330

    Add DynamicAccessController

---
+++ BookShop/Areas/Admin/Controllers/DynamicAccessController.cs
@@ -0,0 +1,38 @@
+﻿using BookShop.Areas.Admin.Data;
+using BookShop.Areas.Admin.Models.ViewModels;
+using BookShop.Areas.Admin.Services;
+using BookShop.Areas.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Areas.Admin.Controllers
+{
+    public class DynamicAccessController : Controller
+    {
+        private readonly IApplicationRoleManager _roleManager;
+        private readonly IMVCActionsDiscoveryService _actionsDiscoveryService;
+
+        public DynamicAccessController(IApplicationRoleManager roleManager, IMVCActionsDiscoveryService actionsDiscoveryService)
+        {
+            _roleManager = roleManager;
+            _actionsDiscoveryService = actionsDiscoveryService;
+        }
+
+        public async Task<IActionResult> Index(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return NotFound();
+
+            var role = await _roleManager.FindClaimsInRolesAsync(id);
+            if (role is null)
+                return NotFound();
+
+            DynamicAccessIndexViewModel dynamicAccessViewModel = new DynamicAccessIndexViewModel
+            {
+                RoleWithClaims = role,
+                SecuredControllerActions = _actionsDiscoveryService.GetAllSecuredControllerActionsWithPolicy(ConstantPolicies.DynamicPermissin)
+            };
+
+            return View(dynamicAccessViewModel);
+        }
+    }
+}
