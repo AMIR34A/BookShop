19516b4acd02a76736cb2e3934b5cd05c9eb13d9
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-11-07 16:56:57 +0330

    Add DynamicAccessViewModel

---
+++ BookShop/Areas/Admin/Models/ViewModels/DynamicAccessViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BookShop.Areas.Identity.Data;
+using BookShop.Models.ViewModels;
+
+namespace BookShop.Areas.Admin.Models.ViewModels;
+
+public class DynamicAccessIndexViewModel
+{
+    public string RoleId { get; set; }
+    public string[] ActionsId { get; set; }
+    public ApplicationRole RoleWithClaims { get; set; }
+    public ICollection<ControllerViewModel> SecuredControllerActions { get; set; }
+}
