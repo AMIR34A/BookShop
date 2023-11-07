26390527a5196deecfc6c9e914cc6d0138df795a
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-11-07 16:03:02 +0330

    Add ConstantPolicies

---
+++ BookShop/Areas/Admin/Data/ConstantPolicies.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Areas.Admin.Data
+{
+    public class ConstantPolicies
+    {
+        public const string DynamicPermissin = nameof(DynamicPermissin);
+        public const string DynamicPermissionClaimType = nameof(DynamicPermissionClaimType);
+    }
+}
