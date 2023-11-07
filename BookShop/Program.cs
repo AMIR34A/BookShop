be9d698ae4ef13bfc7a650685e85f353b521c555
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-11-07 16:05:18 +0330

    Add MVCActionsDiscoveryService service

--- BookShop/Program.cs
+++ BookShop/Program.cs
@@ -62,6 +62,7 @@ builder.Services.AddScoped<IEmailSender, EmailSenderService>();
 builder.Services.AddScoped<ISMSSenderService, SMSSenderService>();
 builder.Services.AddSingleton<IAuthorizationHandler, DateOfBirthAuthorizationHandler>();
 builder.Services.AddSingleton<IAuthorizationHandler, MinimumAgeAuthorizationHandler>();
+builder.Services.AddSingleton<IMVCActionsDiscoveryService, MVCActionsDiscoveryService>();
 builder.Services.AddHttpClient();
 
 builder.Services.AddAuthentication()
