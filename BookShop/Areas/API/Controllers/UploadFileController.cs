b3e17efb9d35d6b5c3271e318d2b2ce56d697cbf
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2024-03-21 22:26:18 +0330

    feat:add UploadFile api

---
+++ BookShop/Areas/API/Controllers/UploadFileController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Areas.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UploadFileController : ControllerBase
+{
+    private readonly IWebHostEnvironment _webHostEnvironment;
+    public UploadFileController(IWebHostEnvironment webHostEnvironment)
+    {
+        _webHostEnvironment = webHostEnvironment;
+    }
+
+    [HttpPost]
+    public async Task<string> UploadImage([FromBody]string base64Image)
+    {
+        var bytes = Convert.FromBase64String(base64Image);
+        string path = $"{_webHostEnvironment.WebRootPath}\\Files\\{Guid.NewGuid()}.jpg";
+        await System.IO.File.WriteAllBytesAsync(path, bytes);
+        return "عکس با موفقیت آپلود شد";
+    }
+}
