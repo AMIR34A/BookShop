e1919d4f6f526e7bd36fbe0f0ca2176d8371b54b
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2024-03-23 22:31:50 +0330

    feat:add UsersAPIController

---
+++ BookShop/Areas/API/Controllers/UsersAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Areas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersAPIController : ControllerBase
+    {
+    }
+}
