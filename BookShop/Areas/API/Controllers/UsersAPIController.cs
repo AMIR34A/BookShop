884e0419490294efb7995239ad6c97287a32875d
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2024-03-24 23:46:47 +0330

    feat:add injection

--- BookShop/Areas/API/Controllers/UsersAPIController.cs
+++ BookShop/Areas/API/Controllers/UsersAPIController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BookShop.Areas.Identity.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BookShop.Areas.API.Controllers
+namespace BookShop.Areas.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersAPIController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersAPIController : ControllerBase
+    private readonly IApplicationUserManager _applicationUserManager;
+    private readonly IApplicationRoleManager _applicationRoleManager;
+
+    public UsersAPIController(IApplicationUserManager applicationUserManager, IApplicationRoleManager applicationRoleManager)
     {
+        _applicationUserManager = applicationUserManager;
+        _applicationRoleManager = applicationRoleManager;
     }
 }
