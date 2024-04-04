92723b6a0122636d635a5e8aaae16baeb123b0d6
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2024-04-04 23:26:03 +0330

    Feat: add APIStatusCode enum

---
+++ BookShop/Areas/API/Models/APIStatusCode.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.Areas.API.Models;
+
+public enum ApiResultStatusCode
+{
+    [Display(Name = "عملیات با موفقیت انجام شد")]
+    Success,
+
+    [Display(Name = "خطایی در سرور رخ داده است")]
+    ServerError,
+
+    [Display(Name = "پارامتر های ارسالی معتبر نیستند")]
+    BadRequest,
+
+    [Display(Name = "یافت نشد")]
+    NotFound,
+
+    [Display(Name = "لیست خالی است")]
+    ListEmpty,
+
+    [Display(Name = "خطایی در پردازش رخ داد")]
+    LogicError,
+
+    [Display(Name = "خطای احراز هویت")]
+    UnAuthorized
+}
