9479a3bcaa19d5960088c12a92e676561bb94e62
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2024-04-14 22:16:41 +0330

    Feat: add BadRequestObjectResultExtentions

---
+++ BookShop/Areas/API/Classes/BadRequestObjectResultExtentions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Areas.API.Classes;
+
+public static class BadRequestObjectResultExtentions
+{
+    public static List<string> GetErrors(this BadRequestObjectResult result)
+    {
+        List<string> message = new List<string>();
+        if (result.Value is SerializableError errors)
+        {
+            var errorMessages = errors.SelectMany(p => (string[])p.Value).Distinct();
+            message.AddRange(errorMessages);
+        }
+        else if (result.Value is IEnumerable<IdentityError> identityErrors)
+        {
+            var errorMessages = identityErrors.Select(p => p.Description).Distinct();
+            message.AddRange(errorMessages);
+        }
+        else
+            message.Add(result.Value.ToString());
+        return message;
+    }
+}
