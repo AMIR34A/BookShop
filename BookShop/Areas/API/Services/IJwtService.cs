51abbcfe88dd8e5f4806f8f732efc5779a800748
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2024-04-23 22:20:08 +0330

    Feat: add JwtService

---
+++ BookShop/Areas/API/Services/IJwtService.cs
@@ -0,0 +1,8 @@
+﻿using BookShop.Areas.Identity.Data;
+
+namespace BookShop.Areas.API.Services;
+
+public interface IJwtService
+{
+    Task<string> GenerateTokenAsync(ApplicationUser user);
+}
---
+++ BookShop/Areas/API/Services/JwtService.cs
@@ -0,0 +1,33 @@
+﻿using BookShop.Areas.Identity.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace BookShop.Areas.API.Services;
+
+public class JwtService : IJwtService
+{
+    private readonly IApplicationUserManager _applicationUserManager;
+    public JwtService(IApplicationUserManager applicationUserManager) => _applicationUserManager = applicationUserManager;
+
+    public async Task<string> GenerateTokenAsync(ApplicationUser user)
+    {
+        return string.Empty;
+    }
+
+    private async Task<IEnumerable<Claim>> GetClaimsAsync(ApplicationUser user)
+    {
+        List<Claim> claims = new()
+        {
+            new Claim(ClaimTypes.Name,user.UserName),
+            new Claim(ClaimTypes.NameIdentifier,user.Id),
+            new Claim(ClaimTypes.MobilePhone,user.PhoneNumber),
+            new Claim("SecurityStampClaimType",user.SecurityStamp),
+            new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+        };
+
+        var roles = await _applicationUserManager.GetRolesAsync(user);
+
+        claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+        return claims;
+    }
+}
