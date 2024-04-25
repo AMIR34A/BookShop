0ce2c464c8309076a3aafc20b5004ad0b349f230
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2024-04-25 23:40:25 +0330

    Fix: edit GenerateTokenAsync method

--- BookShop/Areas/API/Services/JwtService.cs
+++ BookShop/Areas/API/Services/JwtService.cs
@@ -1,17 +1,36 @@
 ﻿using BookShop.Areas.Identity.Data;
+using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
 
 namespace BookShop.Areas.API.Services;
 
 public class JwtService : IJwtService
 {
     private readonly IApplicationUserManager _applicationUserManager;
+
     public JwtService(IApplicationUserManager applicationUserManager) => _applicationUserManager = applicationUserManager;
 
     public async Task<string> GenerateTokenAsync(ApplicationUser user)
     {
-        return string.Empty;
+        byte[] secretKey = Encoding.UTF8.GetBytes("1234567890abcdefg");
+        var signinCredential = new SigningCredentials(new SymmetricSecurityKey(secretKey), SecurityAlgorithms.HmacSha256Signature);
+
+        var tokenDescriotir = new SecurityTokenDescriptor
+        {
+            Issuer = "BookShop.ir",
+            Audience = "BookShop.ir",
+            IssuedAt = DateTime.Now,
+            NotBefore = DateTime.Now,
+            Expires = DateTime.Now.AddMinutes(20),
+            SigningCredentials = signinCredential,
+            Subject = new ClaimsIdentity(await GetClaimsAsync(user))
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var securityToken = tokenHandler.CreateJwtSecurityToken(tokenDescriotir);
+        return tokenHandler.WriteToken(securityToken);
     }
 
     private async Task<IEnumerable<Claim>> GetClaimsAsync(ApplicationUser user)
