faf7f63aefc9de162771cd93eaad2de494a2fe31
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2024-05-07 23:34:56 +0330

    Feat: add CustomExceptionHandlerMiddlewareExtensions

---
+++ BookShop/Areas/API/MiddleWares/CustomExceptionHandlerMiddlewareExtensions.cs
@@ -0,0 +1,48 @@
+﻿using BookShop.Areas.API.Classes;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace BookShop.Areas.API.Middlewares;
+
+public class CustomExceptionHandlerMiddlewareExtensions
+{
+}
+
+public class CustomExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public CustomExceptionHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext httpContext)
+    {
+        List<string> messages = new List<string>();
+        HttpStatusCode httpStatusCode = HttpStatusCode.InternalServerError;
+        ApiResultStatusCode apiResultStatusCode = ApiResultStatusCode.ServerError;
+
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception exception)
+        {
+            var error = new Dictionary<string, string>
+            {
+                ["Exception"] = exception.Message,
+                ["StackTrace"] = exception.StackTrace
+            };
+
+            messages.Add(JsonConvert.SerializeObject(error));
+            var result = new APIResult(false, apiResultStatusCode, messages);
+            var jsonResult = JsonConvert.SerializeObject(result);
+
+            httpContext.Response.StatusCode = (int)httpStatusCode;
+            httpContext.Response.ContentType = "application/json";
+
+            await httpContext.Response.WriteAsync(jsonResult);
+        }
+    }
+}
