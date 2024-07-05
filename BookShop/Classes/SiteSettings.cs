38783b580ac9b9a531585bd873262bfe5176fb9c
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2024-07-05 23:23:10 +0330

    Feat: add SiteSettings class

---
+++ BookShop/Classes/SiteSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace BookShop.Classes;
+
+public class SiteSettings
+{
+    public JWTSettings JWTSettings { get; set; }
+}
+
+public class JWTSettings
+{
+    public string SecretKey { get; set; }
+    public string EncryptKey { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public int NotBeforeMinutes { get; set; }
+    public int ExpirationMinutes { get; set; }
+}
