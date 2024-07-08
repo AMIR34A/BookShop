977068d14ba885210d9447ff9b5d78794c313ecb
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2024-07-08 17:35:39 +0330

    Feat: add SwaggerConfigurationExtensions

---
+++ BookShop/Areas/API/Swagger/SwaggerConfigurationExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Asp.Versioning;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
+
+namespace BookShop.Areas.API.Swagger;
+
+public static class SwaggerConfigurationExtensions
+{
+    public static void AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(setup =>
+        {
+            setup.SwaggerDoc("v1", new OpenApiInfo()
+            {
+                Version = "v1",
+                Title = "BookShop Api",
+                Description = "An api for CRUD operations",
+            });
+
+            setup.DescribeAllParametersInCamelCase();
+
+            setup.DocInclusionPredicate((apiName, apiDescription) =>
+            {
+                if (!apiDescription.TryGetMethodInfo(out MethodInfo methodInfo))
+                    return false;
+                var versions = methodInfo.DeclaringType.
+                                  GetCustomAttribute<ApiVersionAttribute>(true).Versions;
+
+                return versions.Any(apiVersion => apiName.Equals($"v{apiVersion.MajorVersion}"));
+            });
+
+            //var path = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            //var xmlPath = Path.Combine(AppContext.BaseDirectory, path);
+            //setup.IncludeXmlComments(() => new System.Xml.XPath.XPathDocument(xmlPath));
+        });
+    }
+
+    public static void AddSwaggerAndSwaggerUI(this IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(setup =>
+        {
+            setup.SwaggerEndpoint("/swagger/v1/swagger.json", "V1 API");
+        });
+    }
+}
