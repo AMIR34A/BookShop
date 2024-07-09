7fea1211669105847c981d9f2ed1716ff26cec18
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2024-07-09 22:00:16 +0330

    Feat: add RemoveVersionParameter

---
+++ BookShop/Areas/API/Swagger/RemoveVersionParameter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace BookShop.Areas.API.Swagger;
+
+public class RemoveVersionParameter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        var versionParameter = operation.Parameters.FirstOrDefault(parameter => parameter.Name.Equals("version"));
+        if (versionParameter is not null)
+            operation.Parameters.Remove(versionParameter);
+    }
+}
