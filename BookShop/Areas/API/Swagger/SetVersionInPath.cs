9bea0960135d48c223f14b9fa441b72f2ff76f76
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2024-07-09 22:00:33 +0330

    Feat: add SetVersionInPath

---
+++ BookShop/Areas/API/Swagger/SetVersionInPath.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.OpenApi.Models;
+using NuGet.Packaging;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace BookShop.Areas.API.Swagger;
+
+public class SetVersionInPath : IDocumentFilter
+{
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        Dictionary<string, OpenApiPathItem> updatedPath = new();
+
+        foreach (var path in swaggerDoc.Paths)
+            updatedPath.Add(path.Key.Replace("v{version}", swaggerDoc.Info.Version), path.Value);
+
+        swaggerDoc.Paths.Clear();
+       swaggerDoc.Paths.AddRange(updatedPath);
+    }
+}
